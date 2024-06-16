df34f54e98ff1cd737d310e262d90530fe7ccafc
DesperateYuri <101632756+DesperateYuri@users.noreply.github.com>
2024-06-16 20:50:14 +0900

    The second part of the study

---
+++ 04-hypermedia-driven-rest-service/src/main/java/com/github/Yyur1/HateoasApplication.java
@@ -0,0 +1,13 @@
+package com.github.Yyur1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasApplication.class, args);
+    }
+
+}
